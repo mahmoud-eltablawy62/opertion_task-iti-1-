6c137563e38cc5f27ee75b3b165f491638e19c77
mahmoud-eltablawy62 <134718324+mahmoud-eltablawy62@users.noreply.github.com>
2023-07-09 20:10:19 +0300

    task 1_iti

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the num1");
+            float num = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("enter the num1");
+            float num2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"num + num2 = {num + num2}");
+            Console.WriteLine($"num * num2 = {num * num2}");
+            Console.WriteLine($"num / num2 = {num / num2}");
+            Console.WriteLine($"num - num2 = {num - num2}");
+            Console.WriteLine(-1 + 4 * 6);
+        }
+    }
+}
+
