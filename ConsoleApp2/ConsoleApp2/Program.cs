2deacc58ea771b4bff8e46df9878c5cdd11b5548
mahmoud-eltablawy62 <134718324+mahmoud-eltablawy62@users.noreply.github.com>
2023-07-09 20:43:14 +0300

    task1 -> 2

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.WriteLine("-1 + 4 * 6 = " + (-1 + 4 * 6));
+
+            Console.WriteLine("( 35+ 5 ) % 7 = " + ((35 + 5) % 7));
+
+            Console.WriteLine("14 + -4 * 6 / 11 = " + (14 + -4 * 6 / 11));
+
+            Console.WriteLine("2 + 15 / 6 * 1 - 7 % 2 = " + (2 + 15 / 6 * 1 - 7 % 2));
+
+            int x = 10;
+            Console.WriteLine("(x++) + x = " + ((x++) + x));
+
+        }
+    }
+}
